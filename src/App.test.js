9bb331e707f840c90d57723f4c9816629674365f
alejandra <iraicyahh@gmail.com>
2022-01-04 00:34:54 -0500

    version 2

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,27 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import GetArtist from "./hook/GetArtist"
+test('artist debe ser string vacio', () => {
+  const testValue = GetArtist()
+  let artist = testValue[0]
+  console.log(artist)
+  expect(artist).toEqual("")
+})
+test('searchArtist array vacio', () => {
+  const testValue = GetArtist()
+  let searchArtist= testValue[1]
+  expect(searchArtist).toEqual([])
+})
+test('showModal debe ser false al principio', () => {
+  const testValue = GetArtist()
+  let showModal= testValue[2]
+  expect(showModal).toEqual(false)
+})
+test('newMessage debe ser false al principio', () => {
+  const testValue = GetArtist()
+  let newMessage= testValue[3]
+  expect(newMessage).toEqual(false)
+})
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+
+
+
+
---
+++ src/hook/GetArtist.js
@@ -0,0 +1,13 @@
+import React, { useState } from 'react'
+
+const GetArtist = () => {
+  const artist= "";
+  const searchArtist = [];
+  const showModal=false;
+  const newMessage = false;
+  return [
+    artist, searchArtist, showModal, newMessage
+  ]
+}
+
+export default GetArtist
